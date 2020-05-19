79dc97bcf01cdf867bbf518d5c302c9c65580bc9
Petr Bouchal <pbouchal@gmail.com>
2020-05-19 23:48:23 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ mkdir deploy-files
 cp index.html deploy-files
 cp colours.html deploy-files
 
-rm -rf deploy-files
+netlify deploy --prod --dir=deploy-files
 
-netlify deploy --prod --dir=output
+rm -rf deploy-files
